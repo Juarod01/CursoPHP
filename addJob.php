7990a8e73f2b648eb7dfe2fc0d19d707de7ffabb
David Rodriguez <jdavid.rodriguez.e@gmail.com>
2020-01-21 22:40:48 -0500

    Añadiendo archivo addjob.php un ejercicio para conectar BBDD

---
+++ addJob.php
@@ -0,0 +1,26 @@
+<?php
+  var_dump($_POST);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+  <title>Add Job</title>
+</head>
+<body>
+  <h1>Add Job</h1>
+  <form action="addJob.php" method="POST">
+    <label for="">Title:</label>
+    <input type="text" name="title">
+    <br>
+    <label for="">Description:</label>
+    <input type="text" name="description">
+    <br>
+    <button type="submit">Save</button>
+  </form>
+</body>
+</html>
