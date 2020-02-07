5060f4a6ec0267fbc1b0ac7ec83e366bbcff1004
David Rodriguez <jdavid.rodriguez.e@gmail.com>
2020-02-06 23:17:28 -0500

    Creando variables de entorno, para datos de conexion a DB

--- public/index.php
+++ public/index.php
@@ -8,16 +8,19 @@
 
   session_start();
 
+  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+  $dotenv->load();
+
   use Illuminate\Database\Capsule\Manager as Capsule;
   use Aura\Router\RouterContainer;
 
   $capsule = new Capsule;
   $capsule->addConnection([
       'driver'    => 'mysql',
-      'host'      => 'localhost',
-      'database'  => 'cursophp',
-      'username'  => 'root',
-      'password'  => '',
+      'host'      => getenv('DB_HOST'),
+      'database'  => getenv('DB_NAME'),
+      'username'  => getenv('DB_USER'),
+      'password'  => getenv('DB_PASS'),
       'charset'   => 'utf8',
       'collation' => 'utf8_unicode_ci',
       'prefix'    => '',
